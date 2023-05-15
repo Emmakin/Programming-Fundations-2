4e2fd5ac1ab68ad2233c1568d5775b64fbae3242
Emmakin <101441304+Emmakin@users.noreply.github.com>
2023-05-15 01:46:57 +0100

    wrote some detele function to detele list

--- Exercise Files/2 Data Structures/LinkedList/linklist_start.py
+++ Exercise Files/2 Data Structures/LinkedList/linklist_start.py
@@ -51,6 +51,16 @@ class LinkedList(object):
         # TODO: delete an item at given index
         if idx > self.count-1:
             return
+        if idx == 0:
+            self.head = self.head.get_next()
+        else:
+            tempIdx = 0
+            node = self.head
+            while tempIdx < idx - 1:
+                node = node.get_next()
+                tempIdx += 1
+            node.set_next(node.get_next().get_next())
+            self.count -= 1
 
     def dump_list(self):
         tempnode = self.head
@@ -68,12 +78,12 @@ itemlist.insert(15)
 itemlist.dump_list()
 
 # exercise the list
-print("Item count: ", itemlist.get_count())
-print("Finding item: ", itemlist.find(13))
-print("Finding item: ", itemlist.find(78))
+# print("Item count: ", itemlist.get_count())
+# print("Finding item: ", itemlist.find(13))
+# print("Finding item: ", itemlist.find(78))
 
 # delete an item
-# itemlist.deleteAt(3)
-# print("Item count: ", itemlist.get_count())
-# print("Finding item: ", itemlist.find(38))
-# itemlist.dump_list()
+itemlist.deleteAt(3)
+print("Item count: ", itemlist.get_count())
+print("Finding item: ", itemlist.find(38))
+itemlist.dump_list()
