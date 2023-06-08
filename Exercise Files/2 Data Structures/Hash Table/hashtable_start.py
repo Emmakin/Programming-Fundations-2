55f574c7010f456fbf20e53ae51ed3f9e370c661
Emmakin <101441304+Emmakin@users.noreply.github.com>
2023-06-08 09:14:19 +0100

    update

--- Exercise Files/2 Data Structures/Hash Table/hashtable_start.py
+++ Exercise Files/2 Data Structures/Hash Table/hashtable_start.py
@@ -2,15 +2,23 @@
 
 
 # TODO: create a hashtable all at once
-
+item1 = dict({"key1" : 1, "key2" : 2, "key3" : 3})
+print(item1)
 
 # TODO: create a hashtable progressively
-
+item2 = {}
+item2["ade"] = 2
+item2["bola"] = 1
+item2["tope"] = 3
+print(item2)
 
 # TODO: try to access a nonexistent key
-
+# print(item1["key7"])
 
 # TODO: replace an item
-
+item1["key2"] = "two"
+print(item1)
 
 # TODO: iterate the keys and values in the dictionary
+for key, value in item2.items():
+    print("key: ", key , "Value:", value)
