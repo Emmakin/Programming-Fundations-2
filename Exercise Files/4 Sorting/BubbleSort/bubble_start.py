1be6f681e66664d15f74c764bb285a5d3fc96e96
Emmakin <101441304+Emmakin@users.noreply.github.com>
2023-06-23 16:18:49 +0100

    update

--- Exercise Files/4 Sorting/BubbleSort/bubble_start.py
+++ Exercise Files/4 Sorting/BubbleSort/bubble_start.py
@@ -3,6 +3,12 @@
 
 def bubbleSort(dataset):
     # TODO: start with the array length and decrement each time
+    for i in range (len(dataset)-1, 0, -1):
+        for j in range(i):
+            if dataset[j] > dataset[j+1]:
+                temp = dataset[j]
+                dataset[j] = dataset[j+1]
+                dataset[j+1] = temp
 
     print("Current state: ", dataset)
 
