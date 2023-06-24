c97f1661ca5f860b240845e50a316a46136f4b88
Emmakin <101441304+Emmakin@users.noreply.github.com>
2023-06-24 15:02:05 +0100

    updat

--- Exercise Files/5 Searching/unordered_start.py
+++ Exercise Files/5 Searching/unordered_start.py
@@ -5,7 +5,11 @@
 items = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
 
 def find_item(item, itemlist):
-    pass
+    for i in range (i, len(items)):
+        if item == itemlist[i]:
+            return i
+        
+    return None
 
 
 print(find_item(87, items))
