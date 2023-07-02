5f9f5ef215128bf245706a792bc4f66582db41b2
Emmakin <101441304+Emmakin@users.noreply.github.com>
2023-07-02 16:04:59 +0100

    update

--- Exercise Files/6 Other Algorithms/ValueCounter_start.py
+++ Exercise Files/6 Other Algorithms/ValueCounter_start.py
@@ -7,10 +7,14 @@ items = ["apple", "pear", "orange", "banana", "apple",
          "apple", "kiwi", "pear", "apple", "orange"]
 
 # TODO: create a hashtable object to hold the items and counts
-counter = None
+counter = dict()
 
 # TODO: iterate over each item and increment the count for each one
-
+for item in items:
+    if (item in counter.keys()):
+        counter[item] =+ 1
+    else:
+        counter[item] = 1
 
 # print the results
 print(counter)
