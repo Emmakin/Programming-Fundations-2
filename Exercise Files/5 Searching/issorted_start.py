0516623d314261f1517e2b95822c267557666156
Emmakin <101441304+Emmakin@users.noreply.github.com>
2023-07-02 15:50:29 +0100

    update

--- Exercise Files/5 Searching/issorted_start.py
+++ Exercise Files/5 Searching/issorted_start.py
@@ -6,11 +6,12 @@ items2 = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
 
 def is_sorted(itemlist):
     # TODO: using the brute force method
-    for i in range(0, len(itemlist)-1):
-        if (itemlist[i] > itemlist[i+1]):
-            return False 
+    # for i in range(0, len(itemlist)-1):
+    #     if (itemlist[i] > itemlist[i+1]):
+    #         return False 
+    return all (itemlist[i] <= itemlist[i+1] for i in range(len(itemlist)-1))
 
-    return True
+    # return True
 
 print(is_sorted(items1))
 print(is_sorted(items2))
